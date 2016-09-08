97245b896c9a23efa060dc21aeb738fe33ef6673
vaibhav <vaibhav88sharma@gmail.com>
2016-09-08 15:01:08 +1000

    directive.js note app
    directive.js note app

--- AngNoteApp/Scripts/MyScript/directives/title.js
+++ AngNoteApp/Scripts/MyScript/directives/title.js
@@ -1,1 +1,11 @@
-﻿
+﻿angular.module('NoteWrangler').directive('title', function (Category) {
+	return function(scope, element, attrs){	
+		/*$timeout(function(){
+			$(element).tooltip();
+		});	*/
+		
+		scope.$on('$destroy',function(){
+			$(element).tooltip('destroy');
+		});
+	};
+});
