3ccc969d54f550737f791c5ae03f258f7b03a76b
vaibhav <vaibhav88sharma@gmail.com>
2017-09-02 19:36:20 +1000

    before dynamic controls create

--- Angular2WebpackAerryTax/src/app/forms/company-register/company-register.component.ts
+++ Angular2WebpackAerryTax/src/app/forms/company-register/company-register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 //import { NgForm } from '@angular/forms';  // Template Forms
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';  //Reactive Forms
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';  //Reactive Forms
+import 'rxjs/add/operator/debounceTime';
 
 import { CompanyRegister } from './company-register';
 
@@ -13,31 +14,54 @@ export class CompanyRegisterComponent implements OnInit {
 
   crForm :FormGroup;
   cr: CompanyRegister = new CompanyRegister();
+  emailMessage: string;
+
+    private validationMessages = {
+        required: 'Please enter an email address',
+        pattern: 'Please enter a valid email address',
+    };
 
   constructor(private fb: FormBuilder) { }
 
-  ngOnInit(): void {
-    this.crForm = this.fb.group({
-        nameGroup : this.fb.group({
-          name1:['',[Validators.required, Validators.minLength(2)]],
-          name2:['',[Validators.required, Validators.minLength(2)]],
-          //name3:['',[Validators.required, Validators.minLength(2)]],
-        }),
-        commencementDate: ['', Validators.required],
-        companyType: 'public',
-        directorInfoGroup: this.fb.group({
-            surname: ["", Validators.required],
-            directorEmail: ['',[Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]],
-            directorPhone: ['', Validators.required]
-        })
-    });
-  }
+    ngOnInit(): void {
+        this.crForm = this.fb.group({
+            nameGroup : this.fb.group({
+            name1:['',[Validators.required, Validators.minLength(2)]],
+            name2:['',[Validators.required, Validators.minLength(2)]],
+            //name3:['',[Validators.required, Validators.minLength(2)]],
+            }),
+            commencementDate: ['', Validators.required],
+            companyType: 'public',
+            directorInfoGroup: this.fb.group({
+                surname: ["", Validators.required],
+                directorEmail: ['',[Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]],
+                directorPhone: ['', Validators.required]
+            })
+        });
+
+        const emailControl = this.crForm.get('directorInfoGroup.directorEmail');
+        emailControl.valueChanges.debounceTime(1000).subscribe(value => {
+                this.setMessage(emailControl)}
+            );
+        emailControl.statusChanges.subscribe(value => {
+                //console.log(value)
+            }
+        );                
+    }
 
     save() {
       // save(crForm: NgForm) {  // Template Driven
       debugger;
         console.log(this.crForm);
         console.log('Saved: ' + JSON.stringify(this.crForm.value));
+    }
+
+    setMessage(c: AbstractControl): void {
+        this.emailMessage = '';
+        if ((c.touched || c.dirty) && c.errors) {
+            this.emailMessage = Object.keys(c.errors).map(key =>
+                this.validationMessages[key]).join(' ');
+        }
     }
 
     setNotification(notifyVia: string): void {
